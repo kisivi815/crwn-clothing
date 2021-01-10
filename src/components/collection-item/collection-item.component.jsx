38798cdf73dcf3fa47f630b81011f05f0c7d582d
unknown <kisivi_815@hotmail.com>
2021-01-10 21:21:12 +0800

    shop page

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../components/collection-item/collection-item.styles.scss'
+
+const Collectionitem = ({id,name,price,imageUrl}) =>(
+    <div className='collection-item'>
+        <div 
+            className='image'
+            style={{
+                backgroundImage:`url(${imageUrl})`
+            }}
+            />
+            <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+            </div>
+    </div>
+)
+
+export default Collectionitem;
